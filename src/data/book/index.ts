ab69957f67457c988ea7dee41f64c02d2ba24e86
phillypmack <feliperrds@hotmail.com>
2026-08-01 03:27:36 -0300

    Conclui a quarta parte do livro

--- src/data/book/index.ts
+++ src/data/book/index.ts
@@ -13,6 +13,7 @@ import { chapter12 } from "./chapters/ch12";
 import { chapter13 } from "./chapters/ch13";
 import { chapter14 } from "./chapters/ch14";
 import { chapter15 } from "./chapters/ch15";
+import { chapter16 } from "./chapters/ch16";
 
 export { BOOK_SUBTITLE, BOOK_TITLE, BOOK_TOTAL_PAGES, PAGES_PER_CHAPTER, bookChapters, bookParts } from "./outline";
 export { bookReferenceById, bookReferences } from "./bibliography";
@@ -34,6 +35,7 @@ export const bookPages = [
   ...chapter13,
   ...chapter14,
   ...chapter15,
+  ...chapter16,
 ];
 
 export const bookPageByNumber = new Map(bookPages.map((page) => [page.number, page]));
